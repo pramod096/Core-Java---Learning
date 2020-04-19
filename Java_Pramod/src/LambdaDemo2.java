0bb884b92edd249fe10f377976f3f62801e69ee5
Pramod <pramodreddy096@gmail.com>
2020-04-19 11:34:49 +0530

    Lambda Expressions

--- Java_Pramod/src/LambdaDemo2.java
+++ Java_Pramod/src/LambdaDemo2.java
@@ -8,12 +8,12 @@ public class LambdaDemo2 {
 		// TODO Auto-generated method stub
 		NumericTest isFactor = (n , d) -> (n % d ) == 0;
 		
-		if(isFactor.test(10, 2))
-			System.out.println("2 is a factor of 10");
+		if(isFactor.test(10, 3))
+			System.out.println("3 is a factor of 10");
 		if(!isFactor.test(10, 3))
 			System.out.println("3 is not a factor of 10");
 		System.out.println();
-		
+
 		NumericTest lessThan = (f,g) -> (f < g);
 		
 		if(lessThan.test(10, 20))
