b08bb81d8816561034614db453eb5b9c55d0227e
Pramod <pramodreddy096@gmail.com>
2020-04-19 12:13:28 +0530

    Aug 19th

--- Java_Pramod/src/LambdaDemo2.java
+++ Java_Pramod/src/LambdaDemo2.java
@@ -10,8 +10,8 @@ public class LambdaDemo2 {
 		
 		if(isFactor.test(10, 2))
 			System.out.println("2 is a factor of 10");
-		else
-			System.out.println("2 is not a factor of 10");
+		if(!isFactor.test(10, 3))
+			System.out.println("3 is not a factor of 10");
 		System.out.println();
 
 		NumericTest lessThan = (f,g) -> (f < g);
