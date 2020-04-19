08d42acd53cc49ff848a1cea21202a0bb6b905b3
Pramod <pramodreddy096@gmail.com>
2020-04-19 17:51:46 +0530

    Revert "Lambda body as arguments"
    This reverts commit 66b7386d967f72b62749fce2819787bcc9dc10dc.

--- Java_Pramod/src/LambdaDemo2.java
+++ Java_Pramod/src/LambdaDemo2.java
@@ -6,14 +6,7 @@ public class LambdaDemo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		NumericTest isFactor =  (n , d) -> {
-			if((n % d) == 0)
-return true;
-else 
-	return false;
-		};
-		
+		NumericTest isFactor = (n , d) -> (n % d ) == 0;
 		
 		if(isFactor.test(10, 2))
 			System.out.println("2 is a factor of 10");
