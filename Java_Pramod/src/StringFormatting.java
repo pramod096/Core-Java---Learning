5b72f4317d95e1e34c9b174f3592a942ab25c8a1
Pramod <pramodreddy096@gmail.com>
2020-05-01 13:01:21 +0530

    String Formatting

---
+++ Java_Pramod/src/StringFormatting.java
@@ -0,0 +1,12 @@
+
+public class StringFormatting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a = 20;
+			double b = 30;
+			String s = String.format("%010.3f\n %03d", b , a);
+			System.out.println(s);
+	}
+
+}
